303cee9d3c10755b10b518910528cad923312929
Kaonan Micadei <kaonan.micadei@pasqal.com>
2024-04-23 10:58:15 +0200

    Minimal replace function

---
+++ src/tools/replace.py
@@ -0,0 +1,14 @@
+from ..core import Expr, Operator
+from .evaluate import evaluate
+
+
+def replace_core(value, rules: dict):
+    if isinstance(value, Expr):
+        return rules.get(value) or Expr(
+            value.head, *tuple(replace_core(elem, rules) for elem in value.args)
+        )
+    return rules.get(value) or rules.get(Expr(Operator.NONCOMMUTE, value)) or value
+
+
+def replace(expr, rules: dict):
+    return evaluate(replace_core(expr, rules))
