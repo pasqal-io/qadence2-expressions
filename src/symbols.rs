4567d50a22a37cac31cbd3300f7e682a9ea86074
Roland Guichard <roland.guichard@pasqal.com>
2024-06-13 17:54:23 +0100

    Attempt to define a Numeric type.

--- src/symbols.rs
+++ src/symbols.rs
@@ -1,27 +1,38 @@
 use num::Complex;
-use std::ops;
+use std::ops::Add;
 
-pub enum Numerical {
-    Int(i64),
-    Float(f64),
-    Complex(Complex<f64>),
-}
 
+#[derive(Debug)]
 pub enum Numeric {
     Int(i64),
     Float(f64),
     Complex(Complex<f64>),
 }
 
-impl ops::Add<Numeric> for Numeric {
-    // type Output = FooBar;
+impl Add for Numeric {
+    type Output = Numeric;
 
-    fn add(self, _rhs: Numeric) -> Numeric {
-        println!("> Foo.add(Bar) was called");
-	self + _rhs
+    fn add(self, rhs: Self) -> Self {
+	match (self, rhs) {
+	    (Numeric::Int(i1), Numeric::Int(i2)) => Numeric::Int(i1 + i2),
+	    (Numeric::Int(i1), Numeric::Float(f2)) => Numeric::Float(i1 as f64 + f2),
+	    (Numeric::Int(i1), Numeric::Complex(c2)) => Numeric::Complex(Complex::new(i1 as f64, 0.) + c2),
+	    (Numeric::Float(f1), Numeric::Int(i2)) => Numeric::Float(f1 + i2 as f64),
+	    (Numeric::Float(f1), Numeric::Float(f2)) => Numeric::Float(f1 + f2),
+	    (Numeric::Float(f1), Numeric::Complex(c2)) => Numeric::Complex(Complex::new(f1,0.) + c2),
+	    (Numeric::Complex(c1), Numeric::Int(i2)) => Numeric::Complex(c1 + Complex::new(i2 as f64,0.)),
+	    (Numeric::Complex(c1), Numeric::Float(f2)) => Numeric::Complex(c1 + Complex::new(f2, 0.)),
+	    (Numeric::Complex(c1), Numeric::Complex(c2)) => Numeric::Complex(c1+c2),
+	    
+ 	}
     }
 }
 
+impl PartialEq<Numeric> for Numeric {
+    fn eq(&self, other: &Numeric) -> bool {
+	self == other
+    }
+}
 
 
 pub struct Symbol {
@@ -36,7 +47,7 @@ mod tests {
 
     #[test]
     fn test_numeric() {
-        assert_eq!(Operator::ADD.as_str(), "+");
+        // assert_eq!(Numeric::Int(1)+Numeric::Int(2), Numeric::Int(2));
         // assert_eq!(Operator::MUL.as_str(), "*");
         // assert_eq!(Operator::NONCOMMUTE.as_str(), "@");
         // assert_eq!(Operator::POWER.as_str(), "^");
