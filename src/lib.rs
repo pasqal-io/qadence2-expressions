73b558a18597cfe43345f3d8315a76c940ebae43
Roland Guichard <roland.guichard@pasqal.com>
2024-06-14 17:23:57 +0100

    Export modules for access throughout the package.

--- src/lib.rs
+++ src/lib.rs
@@ -1,41 +1,42 @@
-use pyo3::prelude::*;
+// use pyo3::prelude::*;
 
-mod expression;
-// use expression::Operator;
+pub mod operator;
+pub mod symbols;
+pub mod expression;
 
-#[pyclass]
-pub enum Operator {
-    ADD,
-    MUL,
-    NONCOMMUTE,
-    POWER,
-    CALL,
-}
+// #[pyclass]
+// pub enum Operator {
+//     ADD,
+//     MUL,
+//     NONCOMMUTE,
+//     POWER,
+//     CALL,
+// }
 
-impl Operator {
-    pub fn as_str(&self) -> &'static str {
-        match self {
-            Operator::ADD => "+",
-	    Operator::MUL => "*",
-	    Operator::NONCOMMUTE => "@",
-	    Operator::POWER => "^",
-	    Operator::CALL => "call",
-        }
-    }
-}
+// impl Operator {
+//     pub fn as_str(&self) -> &'static str {
+//         match self {
+//             Operator::ADD => "+",
+// 	    Operator::MUL => "*",
+// 	    Operator::NONCOMMUTE => "@",
+// 	    Operator::POWER => "^",
+// 	    Operator::CALL => "call",
+//         }
+//     }
+// }
 
 
-/// Formats the sum of two numbers as string.
-#[pyfunction]
-fn operator() -> PyResult<&'static str> {
-    Ok(Operator::ADD.as_str())
-}
+// /// Formats the sum of two numbers as string.
+// #[pyfunction]
+// fn operator() -> PyResult<&'static str> {
+//     Ok(Operator::ADD.as_str())
+// }
 
-/// A Python module implemented in Rust. The name of this function must match
-/// the `lib.name` setting in the `Cargo.toml`, else Python will not be able to
-/// import the module.
-#[pymodule]
-fn pyexpression(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(operator, m)?)?;
-    Ok(())
-}
+// /// A Python module implemented in Rust. The name of this function must match
+// /// the `lib.name` setting in the `Cargo.toml`, else Python will not be able to
+// /// import the module.
+// #[pymodule]
+// fn pyexpression(m: &Bound<'_, PyModule>) -> PyResult<()> {
+//     m.add_function(wrap_pyfunction!(operator, m)?)?;
+//     Ok(())
+// }
