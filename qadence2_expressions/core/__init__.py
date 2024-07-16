3863c05a2d32f4f101e3a3324ab070e3f3f949e7
Kaonan Micadiei <kaonan.micadei@pasqal.com>
2024-07-16 14:33:42 +0200

    TODO: fix __init__ structure

--- qadence2_expressions/core/__init__.py
+++ qadence2_expressions/core/__init__.py
@@ -1,27 +1,28 @@
 from __future__ import annotations
 
-
+from .ircompiler import irc
 from .primitives import (
-    value,
-    symbol,
-    exp,
-    function,
-    unitary_hermitian_operator,
-    X,
-    Y,
-    Z,
-    Z0,
-    Z1,
-    Xp,
-    Xm,
     RX,
     RY,
     RZ,
-    sin,
+    Z0,
+    Z1,
+    X,
+    Xm,
+    Xp,
+    Y,
+    Z,
     cos,
+    exp,
+    function,
+    set_grid_scale,
+    set_grid_type,
+    set_qubits_positions,
+    sin,
+    symbol,
+    unitary_hermitian_operator,
+    value,
 )
-
-from .ircompiler import irc
 
 __all__ = [
     "irc",
@@ -41,5 +42,8 @@ __all__ = [
     "RY",
     "RZ",
     "sin",
-    "cos"
+    "cos",
+    "set_qubits_positions",
+    "set_grid_type",
+    "set_grid_scale",
 ]
