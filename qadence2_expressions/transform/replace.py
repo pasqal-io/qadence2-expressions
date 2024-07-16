792eac700091de70d5fc22a2d040a5e473d5c29d
Kaonan Micadiei <kaonan.micadei@pasqal.com>
2024-07-16 14:32:14 +0200

    TODO: reimplement tests

--- qadence2_expressions/transform/replace.py
+++ qadence2_expressions/transform/replace.py
@@ -1,16 +1,16 @@
 from __future__ import annotations
 
-from ..core import Expr, Numeric, Numerical, Operator
-from .evaluate import evaluate
+# from ..core import Expr, Numeric, Numerical, Operator
+# from .evaluate import evaluate
 
 
-def replace_core(value: Numeric | Numerical, rules: dict) -> Numeric | Numerical:
-    if isinstance(value, Expr):
-        return rules.get(value) or Expr(
-            value.head, *tuple(replace_core(elem, rules) for elem in value.args)
-        )
-    return rules.get(value) or rules.get(Expr(Operator.NONCOMMUTE, value)) or value
+# def replace_core(value: Numeric | Numerical, rules: dict) -> Numeric | Numerical:
+#     if isinstance(value, Expr):
+#         return rules.get(value) or Expr(
+#             value.head, *tuple(replace_core(elem, rules) for elem in value.args)
+#         )
+#     return rules.get(value) or rules.get(Expr(Operator.NONCOMMUTE, value)) or value
 
 
-def replace(expr: Numeric | Numerical, rules: dict) -> Numeric | Numerical:
-    return evaluate(replace_core(expr, rules))
+# def replace(expr: Numeric | Numerical, rules: dict) -> Numeric | Numerical:
+#     return evaluate(replace_core(expr, rules))
