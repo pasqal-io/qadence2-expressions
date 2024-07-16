16fdf99c573bf4dac9a3ddd8f23cf620b0465149
Kaonan Micadiei <kaonan.micadei@pasqal.com>
2024-07-16 14:34:51 +0200

    TODO: reimplement features

--- qadence2_expressions/transform/evaluate.py
+++ qadence2_expressions/transform/evaluate.py
@@ -1,33 +1,33 @@
 from __future__ import annotations
 
-from typing import Iterable
+# from typing import Iterable
 
-from ..core import Expr, Numeric, Numerical, Operator
-from . import functions as fun
-
-
-def prod(iterable: Iterable) -> Numeric | Numerical:
-    """ "Equivalement of `sum` but for multiplication."""
-
-    acc = 1
-    for el in iterable:
-        acc = acc * el
-    return acc
+# from ..core import Expr, Numeric, Numerical, Operator
+# from . import functions as fun
 
 
-def evaluate(value: Numeric | Numerical) -> Numeric | Numerical:
-    if isinstance(value, Expr):
-        args = [evaluate(elem) for elem in value.args]
-        match value.head:
-            case Operator.TIMES | Operator.NONCOMMUTE:
-                return prod(args)
-            case Operator.PLUS:
-                return sum(args)
-            case Operator.POWER:
-                return args[0] ** args[1]
-            case Operator.CALL:
-                return getattr(fun, args[0])(*args[1:])  # type: ignore
-            case _:
-                raise NotImplementedError(f"Operator {value.head} not implemented.")
+# def prod(iterable: Iterable) -> Numeric | Numerical:
+#     """ "Equivalement of `sum` but for multiplication."""
 
-    return value
+#     acc = 1
+#     for el in iterable:
+#         acc = acc * el
+#     return acc
+
+
+# def evaluate(value: Numeric | Numerical) -> Numeric | Numerical:
+#     if isinstance(value, Expr):
+#         args = [evaluate(elem) for elem in value.args]
+#         match value.head:
+#             case Operator.TIMES | Operator.NONCOMMUTE:
+#                 return prod(args)
+#             case Operator.PLUS:
+#                 return sum(args)
+#             case Operator.POWER:
+#                 return args[0] ** args[1]
+#             case Operator.CALL:
+#                 return getattr(fun, args[0])(*args[1:])  # type: ignore
+#             case _:
+#                 raise NotImplementedError(f"Operator {value.head} not implemented.")
+
+#     return value
