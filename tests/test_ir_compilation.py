53253cd3694738897876229332783b13d749f18d
Kaonan Micadei <k.micadei@gmail.com>
2024-09-17 15:17:55 +0200

    improve compilation test

--- tests/test_ir_compilation.py
+++ tests/test_ir_compilation.py
@@ -1,12 +1,5 @@
 from __future__ import annotations
 
-
-from qadence2_expressions import (
-    parameter,
-    RX,
-    compile_to_model,
-)
-
 from qadence2_ir.types import (
     Alloc,
     AllocQubits,
@@ -18,18 +11,26 @@ from qadence2_ir.types import (
     Support,
 )
 
+from qadence2_expressions import (
+    RX,
+    Z,
+    compile_to_model,
+    parameter,
+)
+
 
 def test_ir_compilation() -> None:
     theta = parameter("theta")
-    expr = RX(theta / 2)(0)
+    expr = Z() * RX(theta / 2)(0)
     model = compile_to_model(expr)
 
     goal = Model(
         register=AllocQubits(num_qubits=1),
-        inputs={'theta': Alloc(1, trainable=False)},
+        inputs={"theta": Alloc(1, trainable=False)},
         instructions=[
-            Assign('%0', Call('mul', 0.5, Load('theta'))),
-            QuInstruct('rx', Support(target=(0,)), Load('%0')),
+            QuInstruct("z", Support.target_all()),
+            Assign("%0", Call("mul", 0.5, Load("theta"))),
+            QuInstruct("rx", Support(target=(0,)), Load("%0")),
         ],
     )
 
