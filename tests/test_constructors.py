aad5a01993a36190c2b76d1f0f49528108a56951
Roland Guichard <roland.guichard@pasqal.com>
2024-10-30 15:49:41 +0100

    Test more constructors.

--- tests/test_constructors.py
+++ tests/test_constructors.py
@@ -3,8 +3,11 @@ from __future__ import annotations
 import pytest
 
 from qadence2_expressions.core.constructors import (
+    array_parameter,
+    array_variable,
     function,
     parameter,
+    projector,
     symbol,
     unitary_hermitian_operator,
     value,
@@ -15,6 +18,8 @@ from qadence2_expressions.core.support import Support
 
 
 def test_constructor() -> None:
+    assert array_parameter("arr_par", size=5) == Expression(Expression.Tag.SYMBOL, "arr_par", size=5)
+    assert array_variable("arr_var", size=5) == Expression(Expression.Tag.SYMBOL, "arr_var", size=5, trainable=True)
     assert value(1) == Expression(Expression.Tag.VALUE, 1)
     with pytest.raises(TypeError):
         value("Non-numerical type.")  # type: ignore [arg-type]
@@ -23,6 +28,7 @@ def test_constructor() -> None:
         symbol("E")
     assert parameter("phi") == Expression(Expression.Tag.SYMBOL, "phi")
     assert variable("phi") == Expression(Expression.Tag.SYMBOL, "phi")
+    assert variable("psi") == Expression(Expression.Tag.SYMBOL, "psi", trainable=True)
 
     assert function("sin", 3.14) == Expression(Expression.Tag.FN, Expression.symbol("sin"), 3.14)
     assert unitary_hermitian_operator("X")(1) == Expression(
